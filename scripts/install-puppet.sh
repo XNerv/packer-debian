1d8f9ffcdc232fd337b707bcec2479fc144cb587
Administrator <thiagosilvaandradesoares@gmail.com>
2016-01-24 21:02:28 -0200

    Add Debian 8.0.0 support

--- scripts/install-puppet.sh
+++ scripts/install-puppet.sh
@@ -6,6 +6,12 @@ if [ $(facter lsbdistcodename) = 'wheezy' ] ; then
     wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
     dpkg -i puppetlabs-release-wheezy.deb
     rm -f puppetlabs-release-wheezy.deb
+
+elif [ $(facter lsbdistcodename) = 'jessie' ] ; then
+    # Prepare puppetlabs repo
+    wget http://apt.puppetlabs.com/puppetlabs-release-jessie.deb
+    dpkg -i puppetlabs-release-jessie.deb
+    rm -f puppetlabs-release-jessie.deb
 fi
 
 apt-get -y update
